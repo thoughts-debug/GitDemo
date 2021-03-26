47686c3ff213b0d2ec3ba17d903fe4a7bb8590b1
TS <ts39909@gmail.com>
2021-03-26 17:12:54 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/APITest.java
@@ -0,0 +1,18 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class APITest 
+{
+	@Test
+	public void postJira()
+	{
+		System.out.println("postJira");
+	}
+	
+	@Test
+	public void deleteTwitter()
+	{
+		System.out.println("deleteTwitter");
+	}
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,19 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest 
+{
+	//testing xml file from maven- integrating maven with testng
+	@Test
+	public void Androidapps()
+	{
+		System.out.println("Androidapps");
+	}
+	
+	@Test
+	public void IOSapps()
+	{
+		System.out.println("IOSapps");
+	}
+}
